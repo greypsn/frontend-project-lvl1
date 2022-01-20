042ea9bbbca0e353c37910c67ed29ad55671790c
Sergey Pavlov <grey_psn@mail.ru>
2022-01-20 15:03:51 +0300

    upload 20-01-2022 15:03

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
-import resdlineSync from 'readline-sync'; 
+import resdlineSync from 'readline-sync';
 
-export function hello(){
-    console.log('Welcome to the Brain Games');
-    const name = resdlineSync.question('Your answer: ');
-    console.log('Hello, ' + name + '!');
+function hello() {
+  console.log('Welcome to the Brain Games');
+  const name = resdlineSync.question('Your answer: ');
+  console.log(`Hello, ${name}!`);
 }
+export default hello;
